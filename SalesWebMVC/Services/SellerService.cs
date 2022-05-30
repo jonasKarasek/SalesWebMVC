1b0db00155bdd2557608dba99f85092dd4ff8a2a
Jonas Karasek <jonas.karasek@hotmail.com>
2022-05-30 15:04:36 -0300

    Preparing sales search navigation views

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -38,9 +38,17 @@ namespace SalesWebMVC.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                //throw new IntegrityException(e.Message);
+                throw new IntegrityException("This seller has sales. You can't delete this");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
